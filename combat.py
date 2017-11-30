5bba8d8759933d2356c5c2bd758904658330d6eb
Shutainzu-Geeto <mathys-c@hotmail.fr>
2017-11-30 11:46:35 +0100

    deplacement valide

--- combat.py
+++ combat.py
@@ -4,6 +4,7 @@ from random import *
 pygame.init()
 from map import *
 from function import *
+from copy import *
 
 #crée une fenêtre 540*720 qui affiche l'image choisis
 fenetre = pygame.display.set_mode((540,720),RESIZABLE)
@@ -70,10 +71,10 @@ position_C4 = position_C4.move(positionPlayer[3][1],positionPlayer[3][0])
 print(position_C4)
 
 player = []
-player.append([C1,position_C1,2])
-player.append([C2,position_C2,2])
-player.append([C3,position_C3,2])
-player.append([C4,position_C4,2])
+player.append([C1,position_C1,1,3,44,40,28,27,18])
+player.append([C2,position_C2,2,2,38,41,40,20,30])
+player.append([C3,position_C3,2,0,41,43,30,29,20])
+player.append([C4,position_C4,2,0,37,35,25,25,40])
 
 D1 = pygame.image.load("img\Characters\Athena\BtlFace - Copie.png").convert()
 D1.set_colorkey((255,255,255))
@@ -100,12 +101,14 @@ position_D4 = position_D4.move(positionOpponent[3][1],positionOpponent[3][0])
 print(position_D4)
 
 opponent = []
-opponent.append([D1,position_D1,2])
-opponent.append([D2,position_D2,2])
-opponent.append([D3,position_D3,2])
-opponent.append([D4,position_D4,2])
+#opponent.append([image,position,deplacement(1 à 3 selon le type_deplacement),type_deplacement(0=infanterie, 1=cavalier, 2=flier, 3=tank)],vie,atk,speed,def,res)
+opponent.append([D1,position_D1,2,0,50,45,32,18,22])
+opponent.append([D2,position_D2,3,1,36,38,36,25,28])
+opponent.append([D3,position_D3,3,1,47,36,25,23,30])
+opponent.append([D4,position_D4,2,2,42,52,18,38,17])
 
 ChoixPerso = 0
+
 continuer=1
 turn = 0 # 0 = player, 1 = opponent
 
@@ -115,10 +118,12 @@ for item in opponent :
     fenetre.blit(item[0],item[1])
 
 turnPlayer = player[:]
+mouvement=copy(turnPlayer[ChoixPerso][2])
+
 #Boucle infinie
 while continuer :
     pygame.display.flip()
-    while not turnPlayer is None :
+    while len(turnPlayer)!=0 :
         fenetre.blit(fond,(0,0))
         for item in player :
             fenetre.blit(item[0],item[1])
@@ -129,41 +134,87 @@ while continuer :
             if event.type == QUIT :
                 continuer=0
             if event.type == KEYUP :
+                eventOccur=False
                 if event.key == K_DOWN :
                     if turnPlayer[ChoixPerso][1].top+90 < 720 :
-                        turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(0,90)
-                        pygame.display.flip()
+                        if deplacementValide(int((turnPlayer[ChoixPerso][1].top+90)/90),int((turnPlayer[ChoixPerso][1].left)/90),t_map,player,opponent,turnPlayer[ChoixPerso][3]):
+                            if t_map[int((turnPlayer[ChoixPerso][1].top+90)/90)][int((turnPlayer[ChoixPerso][1].left)/90)]==3 and turnPlayer[ChoixPerso][2]==0:
+                                if mouvement-2 >=0:
+                                    turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(0,90)
+                                    mouvement-=2
+                                    eventOccur = True
+                            else:
+                                mouvement-=1
+                                turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(0,90)
+                                eventOccur = True
                 if event.key == K_UP :
                     if turnPlayer[ChoixPerso][1].top-90 >= 0 :
-                        turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(0,-90)
-                        pygame.display.flip()
+                        if deplacementValide(int((turnPlayer[ChoixPerso][1].top-90)/90),int((turnPlayer[ChoixPerso][1].left)/90),t_map,player,opponent,turnPlayer[ChoixPerso][3]):
+                            if t_map[int((turnPlayer[ChoixPerso][1].top-90)/90)][int((turnPlayer[ChoixPerso][1].left)/90)]==3 and turnPlayer[ChoixPerso][2]==0:
+                                if mouvement-2 >=0:
+                                    turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(0,-90)
+                                    mouvement-=2
+                                    eventOccur = True
+                            else:
+                                mouvement-=1
+                                turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(0,-90)
+                                eventOccur = True
                 if event.key == K_LEFT :
                     if turnPlayer[ChoixPerso][1].left-90 >= 0 :
-                        turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(-90,0)
-                        pygame.display.flip()
+                        if deplacementValide(int((turnPlayer[ChoixPerso][1].top)/90),int((turnPlayer[ChoixPerso][1].left-90)/90),t_map,player,opponent,turnPlayer[ChoixPerso][3]):
+                            if t_map[int((turnPlayer[ChoixPerso][1].top)/90)][int((turnPlayer[ChoixPerso][1].left-90)/90)]==3 and turnPlayer[ChoixPerso][2]==0:
+                                if mouvement-2 >=0:
+                                    turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(-90,0)
+                                    mouvement-=2
+                                    eventOccur = True
+                            else:
+                                mouvement-=1
+                                turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(-90,0)
+                                eventOccur = True
                 if event.key == K_RIGHT :
                     if turnPlayer[ChoixPerso][1].left+90 < 540 :
-                        turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(90,0)
-                        pygame.display.flip()
+                        if deplacementValide(int((turnPlayer[ChoixPerso][1].top)/90),int((turnPlayer[ChoixPerso][1].left+90)/90),t_map,player,opponent,turnPlayer[ChoixPerso][3]):
+                            if t_map[int((turnPlayer[ChoixPerso][1].top)/90)][int((turnPlayer[ChoixPerso][1].left+90)/90)]==3 and turnPlayer[ChoixPerso][2]==0:
+                                if mouvement-2 >=0:
+                                    turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(90,0)
+                                    mouvement-=2
+                                    eventOccur = True
+                            else:
+                                mouvement-=1
+                                turnPlayer[ChoixPerso][1] = turnPlayer[ChoixPerso][1].move(90,0)
+                                eventOccur = True
                 if event.key == K_TAB :
+                    if mouvement!=copy(turnPlayer[ChoixPerso][2]):
+                        turnPlayer.remove(turnPlayer[ChoixPerso])
                     if ChoixPerso < len(turnPlayer)-1 :
                         ChoixPerso+=1
                     else:
                         ChoixPerso=0
+                    if len(turnPlayer)!=0: 
+                            mouvement=copy(turnPlayer[ChoixPerso][2])
+                if eventOccur:
+                    if turnPlayer[ChoixPerso][3] == 0:
+                        print("Infanterie ",int((turnPlayer[ChoixPerso][1].top)/90),int((turnPlayer[ChoixPerso][1].left)/90))
+                    if turnPlayer[ChoixPerso][3] == 1:
+                        print("Cavalier ",int((turnPlayer[ChoixPerso][1].top)/90),int((turnPlayer[ChoixPerso][1].left)/90))
+                    if turnPlayer[ChoixPerso][3] == 2:
+                        print("Flier ",int((turnPlayer[ChoixPerso][1].top)/90),int((turnPlayer[ChoixPerso][1].left)/90))
+                    if turnPlayer[ChoixPerso][3] == 3:
+                        print("Tank ",int((turnPlayer[ChoixPerso][1].top)/90),int((turnPlayer[ChoixPerso][1].left)/90))
+                    pygame.display.flip()
+                    if mouvement==0:
+                        turnPlayer.remove(turnPlayer[ChoixPerso])
+                        if ChoixPerso < len(turnPlayer)-1 :
+                            ChoixPerso+=1
+                        else:
+                            ChoixPerso=0
+                        if len(turnPlayer)!=0: 
+                            mouvement=copy(turnPlayer[ChoixPerso][2])
     if turn==0 :
         turn=1
         turnPlayer = opponent[:]
+        mouvement=copy(turnPlayer[ChoixPerso][2])
     else :
         turn=0
         turnPlayer = player[:]
-
-"""
-    #for C1
-    fenetre.blit(fond,(0,0))
-    fenetre.blit(C1,position_C1)
-    pygame.display.flip()
-    #for C2
-    fenetre.blit(fond,(0,0))
-    fenetre.blit(C2,position_C2)
-    pygame.display.flip()
-"""
+        mouvement=copy(turnPlayer[ChoixPerso][2])
--- function.py
+++ function.py
@@ -1,3 +1,7 @@
+import pygame
+from pygame.locals import *
+from random import *
+
 def getArrayPos(anyArray,number):
 	arrayreturn = []
 	for i,line in enumerate(anyArray):
@@ -5,3 +9,32 @@ def getArrayPos(anyArray,number):
 			if val == number:
 				arrayreturn.append([i*90,j*90])
 	return arrayreturn
+
+def samePos(x,y,player,opponent):
+	valReturn = True
+	for item in player :
+		if item[1].top/90== x:
+			if item[1].left/90== y:
+				valReturn = False #Joueur sur la case ciblé
+	for item in opponent :
+		if item[1].top/90== x:
+			if item[1].left/90== y:
+				valReturn = False # Enemie sur la case ciblé
+	return valReturn
+	
+def deplacementValide(x,y,map,player,opponent,type_deplacement):
+	
+	if type_deplacement==0:#Infanterie
+		if map[x][y] in (0,3,6,7):
+			return samePos(x,y,player,opponent)
+	elif type_deplacement==1:#Cavalier
+		if map[x][y] in (0,6,7):
+			return samePos(x,y,player,opponent)
+	elif type_deplacement==2:#Flier
+		if map[x][y] in (0,2,3,6,7):
+			return samePos(x,y,player,opponent)
+	else:#Tank
+		if map[x][y] in (0,3,6,7):
+			return samePos(x,y,player,opponent)
+	return False
+	
