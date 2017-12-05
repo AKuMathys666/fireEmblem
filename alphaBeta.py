d4a71c354c66a51ed09e9f6b2ff5ad02ab5e5e75
Mathys Couvreur <Mathys.Couvreur@covage.com>
2017-12-05 14:00:10 +0100

    test

--- alphaBeta.py
+++ alphaBeta.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from random import *
 from caracter import *
 from copy import *
+from itertools import permutations
 
 def alphaBeta(player,opponent, A=-999, B=999):
 
@@ -28,13 +29,14 @@ def getSumHp(listeCharacter):
     return sumHP
 
 def makeTree(player,opponent,turn=1):
+    feuille =(player,opponent,turn)
+    tree=[]
     if turn ==1:
-        tree=[]
-        nbPlayer,playerAlive = alive(player)
-        tree.append(getPermutation(playerAlive))))
+        tree = children(feuille)
+    else:
         for item in tree:
-            print(item[2].display())
-    return 0
+            item = children(item)
+    return tree
 
 def alive(listCharacter):
     nbAlive=0
@@ -45,11 +47,39 @@ def alive(listCharacter):
             nbAlive+=1
     return nbAlive,alive
 
-def getPermutation(playerAlive):
-    permutation=[]
-    countPlayerAlive = len(playerAlive)
-    if countPlayerAlive ==4 :
-        
+def children(feuille):
+    player=feuille[0][:]
+    opponent=feuille[1][:]
 
 
-            
+    
+#def getPermutation(playerAlive,countPlayerAlive):
+#    permutation=[]
+#    if countPlayerAlive ==4 :
+#1234
+#1243
+#1324
+#1342
+#1423
+#1432
+#2134
+#2143
+#2314
+#2341
+#2413
+#2431
+#3124
+#3142
+#3214
+#3241
+#3412
+#3421
+#4123
+#4132
+#4213
+#4231
+#4312
+#4321
+# !4 alternatives
+#feuille => mvt perso 1 => atk perso 1 => mvt perso 2 => atk perso 2 => mvt perso 3 => atk perso 3 => mvt perso 4 => atk perso 4
+
